67e7f02b504a14a36d1b02457c19e6c25234896c
adam <104338817+madamroger007@users.noreply.github.com>
2025-06-25 13:46:27 +0700

    fix: memperbarui dan memperbaiki kode arduino dan diagram
    - Menambahkan broker untuk sensor
    - Mengganti koneksi wifi

--- Arduino/mqtt/mqtt.ino
+++ Arduino/mqtt/mqtt.ino
@@ -4,8 +4,8 @@
 #include <DHT.h>
 
 // Konfigurasi Wi-Fi
-const char* ssid = "Konsol";
-const char* password = "20011116..";
+const char* ssid = "adm";
+const char* password = "adams161121..";
 
 // Konfigurasi MQTT
 const char* mqtt_server = "5ef609a0286140bb9cb1dd89dabdc139.s1.eu.hivemq.cloud";
@@ -16,8 +16,6 @@ const char* mqtt_password = "8HZK!l@%q3?Ah7o0TrxU";
 // Topik MQTT
 const char* topic_siram_status = "otomatis/siram/status";
 const char* topic_kabut_status = "otomatis/kabut/status";
-const char* topic_siram_perintah = "otomatis/siram/perintah";
-const char* topic_kabut_perintah = "otomatis/kabut/perintah";
 const char* topic_kelembapan_tanah = "sensor/kelembapan_tanah";
 const char* topic_suhu_udara = "sensor/suhu_udara";
 const char* topic_kelembapan_udara = "sensor/kelembapan_udara";
@@ -29,8 +27,7 @@ const char* topic_ketinggian_air = "sensor/ketinggian_air";
 #define DHTPIN 4
 #define DHTTYPE DHT11
 #define SOIL_MOISTURE_PIN 33 // A0
-#define TRIG_PIN 26
-#define ECHO_PIN 25
+
 const int DRY_VALUE = 4000;
 const int WET_VALUE = 1200;
 
@@ -38,18 +35,6 @@ WiFiClientSecure espClient;
 PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);
 
-// === Fungsi untuk Mengukur Jarak dari HC-SR04 ===
-float readDistanceCM() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  long duration = pulseIn(ECHO_PIN, HIGH, 30000); // Timeout 30ms
-  float distance = duration * 0.034 / 2;
-  return distance;
-}
 
 // === Callback MQTT ===
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -58,10 +43,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
     message += (char)payload[i];
   }
 
-  if (String(topic) == "otomatis/siram/status") {
+  if (String(topic) == topic_siram_status) {
     digitalWrite(RELAY1_PIN, message == "1" ? LOW : HIGH);
-  } else if (String(topic) == "otomatis/kabut/status") {
+  } else if (String(topic) == topic_kabut_status) {
     digitalWrite(RELAY2_PIN, message == "1" ? LOW : HIGH);
+
   }
 }
 
@@ -89,9 +75,6 @@ void setup_wifi() {
 void setup() {
   Serial.begin(115200);
   dht.begin();
-
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
   pinMode(RELAY1_PIN, OUTPUT);
   pinMode(RELAY2_PIN, OUTPUT);
 
@@ -111,12 +94,13 @@ void reconnect() {
     Serial.print("Menghubungkan ke MQTT...");
     if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
       Serial.println("terhubung");
-      client.subscribe("otomatis/siram/status");
-      client.subscribe("otomatis/kabut/status");
+      client.subscribe(topic_siram_status);
+      client.subscribe(topic_kabut_status);
     } else {
       Serial.print("gagal, rc=");
       Serial.print(client.state());
       Serial.println(" coba lagi dalam 5 detik");
+      
       delay(5000);
     }
   }
@@ -130,31 +114,21 @@ void loop() {
   client.loop();
 
   // Baca sensor
-  const float tinggiTandon = 150.0; // Tinggi tandon dalam cm
   float suhu = dht.readTemperature();
   float kelembapan = dht.readHumidity();
   int nilaiSensorTanah = analogRead(SOIL_MOISTURE_PIN);
-  float jarakAir = readDistanceCM();
+
 
   // Konversi nilai kelembapan tanah ke persentase
   int kelembapanTanah = map(nilaiSensorTanah, DRY_VALUE, WET_VALUE, 0, 100);
   kelembapanTanah = constrain(kelembapanTanah, 0, 100);
 
-// Pastikan jarakAir tidak melebihi tinggiTandon
-jarakAir = constrain(jarakAir, 0, tinggiTandon);
 
-// Hitung ketinggian air
-float ketinggianAir = tinggiTandon - jarakAir;
-
-// Hitung persentase ketinggian air
-int persentaseAir = (ketinggianAir / tinggiTandon) * 100;
-persentaseAir = constrain(persentaseAir, 0, 100);
 
   // Kirim data ke MQTT
-  client.publish("sensor/suhu_udara", String(suhu).c_str(), true);
-  client.publish("sensor/kelembapan_udara", String(kelembapan).c_str(), true);
-  client.publish("sensor/kelembapan_tanah", String(kelembapanTanah).c_str(), true);
-  client.publish("sensor/ketinggian_air", String(persentaseAir).c_str(), true);
+  client.publish(topic_suhu_udara, String(suhu).c_str(), true);
+  client.publish(topic_kelembapan_udara, String(kelembapan).c_str(), true);
+  client.publish(topic_kelembapan_tanah, String(kelembapanTanah).c_str(), true);
 
-  delay(5000); // Kirim data setiap 5 detik
+  delay(3000); // Kirim data setiap 3 detik
 }
